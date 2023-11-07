68ff2fbf89608374067b635b7ff433420d4f775c
ch <chlgur13@kakao.com>
2023-11-07 13:05:59 +0900

    selection_sort

--- selection_sort.py
+++ selection_sort.py
@@ -1,3 +1,16 @@
-def selection_sort(){
-    
-}
+def selection_sort(A) :
+    n = len(A)
+    for i in range(n-1) :  # 0,1,2,3 ... , n-2
+        least = i
+        for j in range(i+1, n) :#i+1,i+2,i+3, ... , n-1
+            if(A[j]<A[least]) :
+                least = j
+        A[i], A[least] = A[least], A[i]
+        printStep(A,i+1)
+
+def printStep(arr,val) :
+    print(" Step %2d = " %val, end='')
+    print(arr)
+
+
+selection_sort([4,3,2,1])
